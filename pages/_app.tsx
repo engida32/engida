6f0b0868a5434d136ca601c4d522a0b97a116928
engida32 <engida32lishan@gmail.com>
2024-08-24 04:36:38 +0300

    chore: Optimize font loading by disabling preloading in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
+const poppins = Poppins({
+  weight: ["100", "200", "400", "700", "900"],
+  preload: false,
+});
 
 export default function App({ Component, pageProps }: any) {
   return (
