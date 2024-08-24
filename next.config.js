3031384afa185e1d07eb36d0434982ba3219ac81
engida32 <engida32lishan@gmail.com>
2024-08-24 05:33:19 +0300

    chore: Update Next.js workflow and configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
+  //make distDir a sibling to the outDir
+  distDir: "dist",
   reactStrictMode: true,
   basePath: "/engida",
   images: {
